4643277a7071654e6582269a7945a536935b4620
Igors78 <oleinikovsigors@gmail.com>
2021-09-04 22:27:51 +0300

    configure api jokes and insult

--- energetic.rb
+++ energetic.rb
@@ -1,41 +1,40 @@
+#!/usr/bin/env ruby
 # frozen_string_literal: true
 
-# This simple bot responds to every "Ping!" message with a "Pong!"
-
 require 'discordrb'
+require 'httparty'
 
-# This statement creates a bot with the specified token and application ID. After this line, you can add events to the
-# created bot, and eventually run it.
-#
-# If you don't yet have a token to put in here, you will need to create a bot account here:
-#   https://discord.com/developers/applications
-# If you're wondering about what redirect URIs and RPC origins, you can ignore those for now. If that doesn't satisfy
-# you, look here: https://github.com/discordrb/discordrb/wiki/Redirect-URIs-and-RPC-origins
-# After creating the bot, simply copy the token (*not* the OAuth2 secret) and put it into the
-# respective place.
-bot = Discordrb::Bot.new token: 'ODgzNjQ0NjE3MTMwNjQ3NTYy.YTM8Lg.ZVuWbG2o1T2jgQyO5KUiwlSqrII'
+bot = Discordrb::Commands::CommandBot.new token: 'ODgzNjQ0NjE3MTMwNjQ3NTYy.YTM8Lg.ZVuWbG2o1T2jgQyO5KUiwlSqrII',
+                                          prefix: '!'
 
-# Here we output the invite URL to the console so the bot account can be invited to the channel. This only has to be
-# done once, afterwards, you can remove this part if you want
-puts "This bot's invite URL is #{bot.invite_url}."
-puts 'Click on it to invite it to your server.'
+bot.mention { |event| event.respond 'Кто-то звал меня на помощь? Попробуй !help' }
 
-# This method call adds an event handler that will be called on any message that exactly contains the string "Ping!".
-# The code inside it will be executed, and a "Pong!" response will be sent to the channel.
+bot.command :joke do |event|
+  url = 'http://rzhunemogu.ru/RandJSON.aspx?CType=11'
+  response = HTTParty.get(url)
+  event << response.body.split('{"content":').last.chomp('}')
+end
+
+bot.command :insult do |event|
+  url = 'https://evilinsult.com/generate_insult.php?lang=ru&type=text'
+  response = HTTParty.get(url)
+  event << response.parsed_response
+end
+
 bot.message(with_text: 'help') do |event|
   event.respond 'Бог поможет!'
 end
 
 bot.message(with_text: 'help!') do |event|
-  event.respond 'По пятницам не подаю!'
+  event.respond 'Зайдите на неделе!'
 end
 
 bot.message(with_text: 'help!!') do |event|
-  event.respond 'Уйди, противный человек!'
+  event.respond 'Ушёл на базу!'
 end
 
 bot.message(with_text: 'help!!!') do |event|
-  event.respond 'Достал, кожаный мешок!'
+  event.respond 'Без формы 0978-345х не обслуживаем!'
 end
 
 # This method call has to be put at the end of your script, it is what makes the bot actually connect to Discord. If you
