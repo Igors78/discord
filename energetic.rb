d704545430240ece6cfc042208f4048f434af3ad
Igors78 <oleinikovsigors@gmail.com>
2021-09-06 16:16:45 +0300

    parse

--- energetic.rb
+++ energetic.rb
@@ -35,7 +35,7 @@ end
 bot.command :weather do |event|
   url = 'https://api.openweathermap.org/data/2.5/weather?q=Wolfsburg&lang=ru&units=metric&appid=ec8a036c5345c0a48bf07fd7e218878c'
   response = HTTParty.get(url)
-  response.parsed_response
+  JSON.parse response, symbolize_names: true
   event << response.body
 end
 
