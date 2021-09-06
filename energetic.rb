c1e95947d72eef47a3a1a0af5c3c396e7678590e
Igors78 <oleinikovsigors@gmail.com>
2021-09-06 16:11:04 +0300

    metric units

--- energetic.rb
+++ energetic.rb
@@ -33,7 +33,7 @@ bot.command :help do |event|
 end
 
 bot.command :weather do |event|
-  url = 'https://api.openweathermap.org/data/2.5/weather?q=Wolfsburg&lang=ru&appid=ec8a036c5345c0a48bf07fd7e218878c'
+  url = 'https://api.openweathermap.org/data/2.5/weather?q=Wolfsburg&lang=ru&units=metric&appid=ec8a036c5345c0a48bf07fd7e218878c'
   response = HTTParty.get(url)
   response.parsed_response
   event << response
